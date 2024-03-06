2ec8ffa5f0d1cab80ae5d6fdda1709d191edb91c
Tecelli Eriz Akintug <tecelli.e.a@gmail.com>
2024-03-06 14:18:15 +0100

    Added temporary restart button

--- objects/pause_menu/pause_menu.gd
+++ objects/pause_menu/pause_menu.gd
@@ -16,3 +16,9 @@ func _on_resumed():
 	visible = false
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
+
+# TODO: Reload current world instead of whole scene
+func _on_Restart_pressed():
+	get_tree().reload_current_scene()
+	Physics.set_gravity_direction(Vector3.DOWN)
+	Pause.set_pause(false)
--- scripts/auto_load/pause.gd
+++ scripts/auto_load/pause.gd
@@ -3,6 +3,8 @@ extends Node
 signal paused()
 signal resumed()
 
+var is_paused: bool = false
+
 func _ready():
 	pause_mode = Node.PAUSE_MODE_PROCESS
 
@@ -10,11 +12,14 @@ func _process(_delta: float):
 	if Input.is_action_just_pressed("pause"):
 		toggle_pause()
 
-func toggle_pause():
+func set_pause(new_pause: bool):
 	var tree := get_tree()
-	tree.paused = not tree.paused
-	if tree.paused:
+	is_paused = new_pause
+	tree.paused = is_paused
+	if is_paused:
 		emit_signal("paused")
 	else:
 		emit_signal("resumed")
-	
+
+func toggle_pause():
+	set_pause(not is_paused)
